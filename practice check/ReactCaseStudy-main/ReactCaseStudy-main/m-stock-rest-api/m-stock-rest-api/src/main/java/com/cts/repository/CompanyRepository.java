a6d34cca51fa36fe98649cc6112857f5ed57e371
yashshh <37655267+yashshh@users.noreply.github.com>
2021-02-18 11:38:52 +0530

    Delete practice check directory

--- practice check/ReactCaseStudy-main/ReactCaseStudy-main/m-stock-rest-api/m-stock-rest-api/src/main/java/com/cts/repository/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cts.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cts.model.CompanyDetails;
-
-public interface CompanyRepository extends JpaRepository<CompanyDetails, Integer>{
-
-}
--- practice check/ReactCaseStudy-main/ReactCaseStudy-main/m-stock-rest-api/m-stock-rest-api/src/main/java/com/cts/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.cts.service;
-
-import com.cts.model.LoginRequest;
-import com.cts.model.User;
-
-public interface UserService {
-
-	public User getUserByEmailIdAndPassword(LoginRequest loginRequest);
-}
